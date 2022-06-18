e9064eed4edd585150a593bb919d8f89563caa26
Simoni Accioly Vieira <simoni.vieira@al.infnet.edu.br>
2022-06-18 15:33:46 -0300

    Atualizacao

---
+++ SpotifyLite.Repository/Mapping/AccountMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SpotifyLite.Domain.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLite.Repository.Mapping
+{
+    public class AccountMapping : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+            builder.HasKey(x => x.Id);
+            builder.Property(x =>x.Id).
+            builder.Property(x => x.Email);
+            builder.Property(x => x.DataLancamento).IsRequired();
+        }
+    }
+}
