edf036afa73851514f2e3478e4d363827c299821
Simoni Accioly Vieira <simoni.vieira@al.infnet.edu.br>
2022-06-18 14:15:24 -0300

    updtateaula3

---
+++ SpotifyLite.Repository/Repository/AlbumRepository.cs
@@ -0,0 +1,21 @@
+﻿using SpotifyLite.Domain.Album;
+using SpotifyLite.Domain.Album.Repository;
+using SpotifyLite.Repository.Context;
+using SpotifyLite.Repository.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyLite.Repository.Repository
+{
+    public class AlbumRepository : Repository<Album>, IAlbumRepository
+    {
+        public AlbumRepository(SpotifyContext context) : base(context)
+        {
+            
+        }
+
+    }
+}
